bf2e1da1a9b0ea802290eebbd1b4d85ab3cddb75
xgqfrms <xgqfrms@outlook.com>
2022-11-29 01:45:50 +0800

    feat: read file in nested folder ✅

--- test/index.cjs
+++ test/index.cjs
@@ -3,17 +3,17 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-console.log(`cjs`)
+// console.log(`cjs`);
 
-// async function calculateSalesFiles(files) {
-//   let result = 0;
-//   for (const file of files) {
-//     const jsonString = fs.readFile(file);
-//     const obj = JSON.parse(jsonString);
-//     result += obj.total;
-//   }
-//   return result;
-// }
+async function calculateSalesFiles(files) {
+  let result = 0;
+  for (const file of files) {
+    const jsonString = await fs.readFile(file);
+    const obj = JSON.parse(jsonString);
+    result += obj.total;
+  }
+  return result;
+}
 
 // async function findSalesFiles(folderName) {
 //   let files = [];
@@ -51,36 +51,68 @@ async function findSalesFiles(folderName, arr = []) {
   return files;
 }
 
-
 async function main() {
   const salesDir = path.join(__dirname, 'stores');
-  // const salesTotalsDir = path.join(__dirname, 'salesTotals');
-  // try {
-  //   await fs.mkdir(salesTotalsDir);
-  //   console.log(`✅ create folder`);
-  // } catch (error) {
-  //   console.log(`❌ folder already exist`, error);
-  // }
+  const salesTotalsDir = path.join(__dirname, 'salesTotals');
+  try {
+    await fs.mkdir(salesTotalsDir);
+    console.log(`✅ create folder`);
+  } catch (error) {
+    console.log(`❌ folder already exist`, error);
+    // ❌ folder already exist [Error: EEXIST: file already exists, mkdir '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals'] {
+    //   errno: -17,
+    //   code: 'EEXIST',
+    //   syscall: 'mkdir',
+    //   path: '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals'
+    // }
+  }
   const salesFiles = await findSalesFiles(salesDir);
   console.log(`salesFiles =`, salesFiles);
-  // const salesTotal = await calculateSalesFiles(salesFiles);
-  // const report = {
-  //   salesTotal,
-  //   // totalStores: salesFiles.length,
-  //   files: salesFiles.length,
-  // };
-  // const reportPath = path.join(salesTotalsDir, 'report.json');
-  // try {
-  //   await fs.unlink(reportPath);
-  // } catch (error) {
-  //   console.log(`❌ delete file error`, error);
-  // }
-  // await fs.writeFile(reportPath, JSON.stringify(report, null, 4));
-  // console.log(`write finished 🚀`);
+  const salesTotal = await calculateSalesFiles(salesFiles);
+  const report = {
+    salesTotal,
+    // totalStores: salesFiles.length,
+    files: salesFiles.length,
+  };
+  const reportPath = path.join(salesTotalsDir, 'report.json');
+  try {
+    await fs.unlink(reportPath);
+  } catch (error) {
+    console.log(`❌ delete file error`, error);
+    // ❌ delete file error [Error: ENOENT: no such file or directory, unlink '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals/report.json'] {
+    //   errno: -2,
+    //   code: 'ENOENT',
+    //   syscall: 'unlink',
+    //   path: '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals/report.json'
+    // }
+  }
+  await fs.writeFile(reportPath, JSON.stringify(report, null, 4));
+  console.log(`write finished 🚀`);
 }
 
 main();
 
+
+/* 
+
+$ node ./index.cjs
+❌ folder already exist [Error: EEXIST: file already exists, mkdir '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals'] {
+  errno: -17,
+  code: 'EEXIST',
+  syscall: 'mkdir',
+  path: '/Users/xgqfrms-mm/Documents/github/node-essentials/test/salesTotals'
+}
+salesFiles = [
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/201/sales.json',
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/202/sales.json',
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/2022/11/11/markdown.json',
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/203/sales.json',
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/204/sales.json',
+  '/Users/xgqfrms-mm/Documents/github/node-essentials/test/stores/bug.json'
+]
+write finished 🚀
+
+*/
 
 /* 
 
---
+++ test/readme.md
@@ -0,0 +1,35 @@
+# WTF
+
+> it works!
+
+## 剧集
+
+2020年10月12日
+
+15. 如何使用 Node.js创建文件和目录 [15/26]
+
+https://learn.microsoft.com/zh-cn/shows/beginners-series-to-nodejs/how-to-create-files-and-directories-with-nodejs-15-of-26
+
+16. 如何使用 Node.js 读取和更新 JSON 文件 [16/26] ✅
+
+https://learn.microsoft.com/zh-cn/shows/beginners-series-to-nodejs/how-to-read-and-update-json-files-with-nodejs-16-of-26
+
+
+
+
+## refs
+
+> Node.js 入门系列
+
+<!-- https://aka.ms/NodeBeginnerSeries -->
+
+https://learn.microsoft.com/zh-cn/shows/beginners-series-to-nodejs
+
+
+> 使用 Node.js 构建 JavaScript 应用程序
+
+<!-- https://aka.ms/LearnNode.js -->
+
+https://learn.microsoft.com/zh-cn/training/paths/build-javascript-applications-nodejs/
+
+
