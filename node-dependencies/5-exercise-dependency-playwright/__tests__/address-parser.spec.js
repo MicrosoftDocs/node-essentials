e3f9141aa0644f56045b678df3f1e0d0aa89a745
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2023-11-23 03:05:46 +0000

    fix: apply code review suggestion

--- node-dependencies/5-exercise-dependency-playwright/__tests__/address-parser.spec.js
+++ node-dependencies/5-exercise-dependency-playwright/__tests__/address-parser.spec.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
-const { parse } = require('../address-parser');
+const { addressParser } = require('../address-parser');
 
 test('Address Parser', () => {
-    const result = parse(
+    const result = addressParser(
         'order: 3 books to address: 112 street city here is my payment info: cardnumber'
     );
 
