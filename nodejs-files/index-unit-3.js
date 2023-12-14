ce0f0d4b368189b445291efedac6e1324b658aed
Dina Berry (MSFT) <diberry@microsoft.com>
2023-12-14 20:44:21 +0000

    updates

--- nodejs-files/index-unit-3.js
+++ nodejs-files/index-unit-3.js
@@ -1,25 +1,39 @@
 const path = require("path");
 const fs = require("fs").promises;
 
-async function findFiles(folderName) {
+async function findSalesFiles(folderName) {
 
-    let results = []
+  // (1) Add an array at the top, to hold the paths to all the sales files that the program finds.
+  let results = [];
 
-    results.push(`${folderName}`);
+  const relativePathToFolder = path.join(__dirname, folderName);
 
-    const relativePathToFolder = path.join(__dirname, folderName);
-    const items = await fs.readdir(relativePathToFolder, { withFileTypes: true });
+  // (2) Read the currentFolder with the `readdir` method. 
+  const items = await fs.readdir(relativePathToFolder, { withFileTypes: true });
 
-    for (const item of items) {
-        if (item.isDirectory()) {
-            const resultsReturned = await findFiles(`${folderName}/${item.name}`);
-            results = results.concat(resultsReturned);
-        } else {
-            results.push(`${folderName}/${item.name}`);
-        }
+  // (3) Add a block to loop over each item returned from the `readdir` function using the asynchronous `for...of` loop. 
+  for (const item of items) {
+
+    // (4) Add an `if` statement to determine if the item is a file or a directory. 
+    if (item.isDirectory()) {
+
+      // (5) If the item is a directory, _resursively call the function `findSalesFiles` again, passing in the path to the item. 
+      const resultsReturned = await findSalesFiles(`${folderName}/${item.name}`);
+      results = results.concat(resultsReturned);
+    } else {
+      // (6) If it's not a directory, add a check to make sure the item name matches *sales.json*.
+      if (item.name === "sales.json") {
+        results.push(`${folderName}/${item.name}`);
+      }
     }
+  }
 
-    return results;
+  return results;
 }
 
-findFiles("stores").then((results) => console.log(results));
+async function main() {
+  const results = await findSalesFiles("stores");
+  console.log(results);
+}
+
+main();
