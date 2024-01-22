bb0a06417f3b82b091ebb82ee1d6834301c74580
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-22 19:10:36 +0000

    chore: Update path syntax and fix code formatting

--- nodejs-files/3-exercise-work-file-system/index-unit-3.js
+++ nodejs-files/3-exercise-work-file-system/index-unit-3.js
@@ -2,29 +2,28 @@
 // index-unit-5.js which has the __dirname and path.join needed
 // to resolve the path correctly.
 
-const path = require("path");
-const fs = require("fs").promises;
+const path = require('path');
+const fs = require('fs').promises;
 
 async function findSalesFiles(folderName) {
-
   // (1) Add an array at the top, to hold the paths to all the sales files that the program finds.
   let results = [];
 
-  // (2) Read the currentFolder with the `readdir` method. 
+  // (2) Read the currentFolder with the `readdir` method.
   const items = await fs.readdir(folderName, { withFileTypes: true });
 
-  // (3) Add a block to loop over each item returned from the `readdir` function using the asynchronous `for...of` loop. 
+  // (3) Add a block to loop over each item returned from the `readdir` function using the asynchronous `for...of` loop.
   for (const item of items) {
-
-    // (4) Add an `if` statement to determine if the item is a file or a directory. 
+    // (4) Add an `if` statement to determine if the item is a file or a directory.
     if (item.isDirectory()) {
-
-      // (5) If the item is a directory, _resursively call the function `findSalesFiles` again, passing in the path to the item. 
-      const resultsReturned = await findSalesFiles(`${folderName}/${item.name}`);
+      // (5) If the item is a directory, _resursively call the function `findSalesFiles` again, passing in the path to the item.
+      const resultsReturned = await findSalesFiles(
+        `${folderName}/${item.name}`,
+      );
       results = results.concat(resultsReturned);
     } else {
       // (6) If it's not a directory, add a check to make sure the item name matches *sales.json*.
-      if (item.name === "sales.json") {
+      if (item.name === 'sales.json') {
         results.push(`${folderName}/${item.name}`);
       }
     }
@@ -34,7 +33,7 @@ async function findSalesFiles(folderName) {
 }
 
 async function main() {
-  const results = await findSalesFiles("stores");
+  const results = await findSalesFiles('stores');
   console.log(results);
 }
 
--- nodejs-files/index-unit-4.js
+++ nodejs-files/index-unit-4.js
@@ -1,7 +1,7 @@
-const path = require("path");
+const path = require('path');
 
-const currentPath = path.parse("stores/201/sales.json")
+const currentPath = path.parse('stores/201/sales.json');
 console.log(currentPath);
 
-const fullPath = path.join(__dirname, "stores", "201", "/sales.json");
+const fullPath = path.join(__dirname, 'stores', '201', '/sales.json');
 console.log(path.parse(fullPath));
