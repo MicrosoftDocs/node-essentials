f97be2d8fcfbfbdce395b7e6b634e11b31a8bba9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-23 21:03:57 +0000

    fix: format

--- node-dependencies/7-exercise-dependency-management/index.js
+++ node-dependencies/7-exercise-dependency-management/index.js
@@ -1,16 +1,19 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const _ = require('lodash');
 const path = require('path');
 const fs = require('fs');
 
 async function run() {
-  const response = await fetch("https://dev.to/api/articles?state=rising");
+  const response = await fetch('https://dev.to/api/articles?state=rising');
   const json = await response.json();
-  const sorted = _.sortBy(json, ["public_reactions_count"], ['desc']);
+  const sorted = _.sortBy(json, ['public_reactions_count'], ['desc']);
   const top3 = _.take(sorted, 3);
 
   const filePrefix = new Date().toISOString().split('T')[0];
-  fs.writeFileSync(path.join(__dirname, `${filePrefix}-feed.json`), JSON.stringify(top3, null, 2));
+  fs.writeFileSync(
+    path.join(__dirname, `${filePrefix}-feed.json`),
+    JSON.stringify(top3, null, 2),
+  );
 }
 
 run();
