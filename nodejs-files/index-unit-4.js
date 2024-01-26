189e755e9d5c1355b15dc60c7d3c611758608bb4
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-01-26 01:03:42 +0000

    chore: Delete unnecessary code in index-unit-4.js

--- nodejs-files/index-unit-4.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-const currentPath = path.parse('stores/201/sales.json');
-console.log(currentPath);
-
-const fullPath = path.join(__dirname, 'stores', '201', '/sales.json');
-console.log(path.parse(fullPath));
