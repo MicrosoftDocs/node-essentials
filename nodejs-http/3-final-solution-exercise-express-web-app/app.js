aab4d2906701b57c06fb0a03b3765c788a1eae24
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-09 23:40:12 +0000

    chore: setup module 05 and add editorconfig, package.json, and eslint configuration files for the sample

---
+++ nodejs-http/3-final-solution-exercise-express-web-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/products', (req, res) => {
+    const products = [
+        { id: 1, name: 'hammer' },
+        { id: 2, name: 'screwdriver' },
+        { id: 3, name: 'wrench' },
+    ];
+
+    res.json(products);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}! http://localhost:${port}/`);
+});
