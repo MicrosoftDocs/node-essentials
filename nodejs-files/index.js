841f4b80b157b13a53b68ccd94eb87632b9bd1aa
sitinajihahhh <149866491+sitinajihahhh@users.noreply.github.com>
2024-02-19 16:28:44 +0000

    Add index.js

---
+++ nodejs-files/index.js
@@ -0,0 +1,36 @@
+const fs = require("fs").promises;
+
+async function findSalesFiles(folderName) {
+
+  // (1) Add an array at the top, to hold the paths to all the sales files that the program finds.
+  let results = [];
+
+  // (2) Read the currentFolder with the `readdir` method. 
+  const items = await fs.readdir(folderName, { withFileTypes: true });
+
+  // (3) Add a block to loop over each item returned from the `readdir` method using the asynchronous `for...of` loop. 
+  for (const item of items) {
+
+    // (4) Add an `if` statement to determine if the item is a file or a directory. 
+    if (item.isDirectory()) {
+
+      // (5) If the item is a directory, recursively call the function `findSalesFiles` again, passing in the path to the item. 
+      const resultsReturned = await findSalesFiles(`${folderName}/${item.name}`);
+      results = results.concat(resultsReturned);
+    } else {
+      // (6) If it's not a directory, add a check to make sure the item name matches *sales.json*.
+      if (item.name === "sales.json") {
+        results.push(`${folderName}/${item.name}`);
+      }
+    }
+  }
+
+  return results;
+}
+
+async function main() {
+  const results = await findSalesFiles("stores");
+  console.log(results);
+}
+
+main();
