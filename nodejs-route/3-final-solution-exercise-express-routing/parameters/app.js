b2ac402e33dbdc81d2ef056de7f98553d80d5977
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-02-19 23:36:49 +0000

    chore: include final solution for exercise unit 3 for routing course

---
+++ nodejs-route/3-final-solution-exercise-express-routing/parameters/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const products = [
+  {
+    id: 1,
+    name: "Ivanhoe",
+    author: "Sir Walter Scott",
+  },
+  {
+    id: 2,
+    name: "Colour Magic",
+    author: "Terry Pratchett",
+  },
+  {
+    id: 3,
+    name: "The Bluest eye",
+    author: "Toni Morrison",
+  },
+];
+
+app.get("/", (req, res) => res.send("Hello API!"));
+
+app.get("/products/:id", (req, res) => {
+  res.json(products.find(p => p.id === +req.params.id));
+});
+
+app.get("/products", (req, res) => {
+  const page = +req.query.page;
+  const pageSize = +req.query.pageSize;
+
+  if (page && pageSize) {
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    res.json(products.slice(start, end));
+  } else {
+    res.json(products);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
