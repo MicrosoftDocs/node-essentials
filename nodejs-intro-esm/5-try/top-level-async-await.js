c8eb4dc506a2477dfe880cd5e3357a9368586728
Dina Berry (MSFT) <diberry@microsoft.com>
2024-05-04 12:49:14 -0700

    Intro to Node - unit 5 code update (#100)
    * Intro to Node: module 5 code snippets (ESM)
    
    * remove 5 from intro

---
+++ nodejs-intro-esm/5-try/top-level-async-await.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch';
+
+console.log(`start`);
+try{
+    const res = await fetch('https://github.com/MicrosoftDocs/node-essentials');
+
+    console.log('statusCode:', res.status);
+
+} catch( error ){
+   console.log(`error: ${error}`);
+}
+console.log(`end`);
+
