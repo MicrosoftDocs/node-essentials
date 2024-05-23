20860058b1e06b962467c27bbb1e9964a6467f05
Dina Berry (MSFT) <diberry@microsoft.com>
2024-05-23 21:40:39 +0000

    fix for esm

--- nodejs-intro-esm/5-try/top-level-async-await.js
+++ nodejs-intro-esm/5-try/top-level-async-await.js
@@ -1,13 +1,11 @@
 import fetch from 'node-fetch';
 
 console.log(`start`);
-try{
-    const res = await fetch('https://github.com/MicrosoftDocs/node-essentials');
+try {
+  const res = await fetch('https://github.com/MicrosoftDocs/node-essentials');
 
-    console.log('statusCode:', res.status);
-
-} catch( error ){
-   console.log(`error: ${error}`);
+  console.log('statusCode:', res.status);
+} catch (error) {
+  console.log(`error: ${error}`);
 }
 console.log(`end`);
-
