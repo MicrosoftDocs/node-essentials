6bc32d52b0a9fc2e6fd0b9b790e9dbdac62dba4a
Dina Berry <diberry@microsoft.com>
2024-08-19 15:03:38 -0700

    remove dist

--- unit-testing/dist/mock-function/data/verify.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.inputVerified = inputVerified;
-// input-verified.ts
-const model_schema_1 = require("./model-schema");
-function inputVerified(doc) {
-    const result = model_schema_1.RawInputSchema.safeParse(doc);
-    return result.success;
-}
