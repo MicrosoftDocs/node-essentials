f3e3b7c7552f197c6ffcb31fed5cf84490990003
Dina Berry <diberry@microsoft.com>
2024-08-22 09:14:58 -0700

    boilerplate

---
+++ unit-testing/src/test-boilerplate/boilerplate-with-mock.spec.ts
@@ -0,0 +1,30 @@
+// boilerplate-with-mock.spec.ts
+
+// Mock the dependencies
+jest.mock('../mock-function/data/connect-to-cosmos', () => ({
+  myFunctionToMock: jest.fn()
+}));
+
+describe('nameOfGroupOfTests', () => {
+
+  beforeEach(() => {
+    // Clear all mocks before each test
+    jest.clearAllMocks();
+
+    // Other setup required before each test
+  });
+
+  it('should <do something> if <situation is present>', async () => {
+    
+    // Arrange
+    // - set up the test data and the expected result
+
+    // Act 
+    // - call the function to test
+
+    // Assert
+    // - check the state: result returned from function 
+    // - check the behavior: dependency function calls
+
+  });
+});
