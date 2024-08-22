6210766f71389b7c1eb53cc3c7cd22e69bb34c6d
Dina Berry <diberry@microsoft.com>
2024-08-22 09:39:21 -0700

    update test file

--- unit-testing/src/mock-function/lib/insert.spec.ts
+++ unit-testing/src/mock-function/lib/insert.spec.ts
@@ -11,24 +11,26 @@ import {
 import { inputVerified } from '../data/verify';
 import { insertDocument } from './insert';
 
-// Mock the dependencies
+// Mock app dependencies for Cosmos DB setup
 jest.mock('../data/connect-to-cosmos', () => ({
   connectToContainer: jest.fn(),
   getUniqueId: jest.fn().mockReturnValue('unique-id'),
-  Container: jest.fn(),
 }));
 
+// Mock app dependencies for input verification
 jest.mock('../data/verify', () => ({
   inputVerified: jest.fn(),
 }));
 
 describe('insertDocument', () => {
+  // Mock the Cosmo DB Container object
   let mockContainer: jest.Mocked<Container>;
 
   beforeEach(() => {
     // Clear all mocks before each test
     jest.clearAllMocks();
 
+    // Mock the Cosmos DB Container create method
     mockContainer = {
       items: {
         create: jest.fn(),
@@ -37,17 +39,20 @@ describe('insertDocument', () => {
   });
 
   it('should return verification error if input is not verified', async () => {
+
+    // Arrange - Mock the input verification function to return false
     jest.mocked(inputVerified).mockReturnValue(false);
 
-    // wrong shape of doc
+    // Arrange - wrong shape of doc on purpose
     const doc = { name: 'test' };
 
+    // Act - Call the function to test
     const insertDocumentResult = await insertDocument(
       mockContainer,
       doc as unknown as RawInput,
     );
 
-    // State verification: Check the result when verification fails
+    // Assert - State verification: Check the result when verification fails
     if (isVerificationErrors(insertDocumentResult)) {
       expect(insertDocumentResult).toEqual({
         message: 'Verification failed',
@@ -56,49 +61,58 @@ describe('insertDocument', () => {
       throw new Error('Result is not of type VerificationErrors');
     }
 
-    // Behavior verification: Ensure create method was not called
+    // Assert - Behavior verification: Ensure create method was not called
     expect(mockContainer.items.create).not.toHaveBeenCalled();
   });
 
   it('should insert document successfully', async () => {
+
+    // Arrange - create input and expected result data
     const { input, result }: { input: RawInput; result: Partial<DbDocument> } =
       createTestInputAndResult();
 
+    // Arrange - mock the input verification function to return true
     (inputVerified as jest.Mock).mockReturnValue(true);
     (mockContainer.items.create as jest.Mock).mockResolvedValue({
       resource: result,
     });
 
+    // Act - Call the function to test
     const insertDocumentResult = await insertDocument(mockContainer, input);
 
-    // State verification: Check the result when insertion is successful
+    // Assert - State verification: Check the result when insertion is successful
     expect(insertDocumentResult).toEqual(result);
 
-    // Behavior verification: Ensure create method was called with correct arguments
+    // Assert - Behavior verification: Ensure create method was called with correct arguments
     expect(mockContainer.items.create).toHaveBeenCalledTimes(1);
   });
 
   it('should return error if db insert fails', async () => {
+
+    // Arrange - create input and expected result data
     const { input } = createTestInputAndResult();
 
+    // Arrange - mock the input verification function to return true
+    (inputVerified as jest.Mock).mockReturnValue(true);
+
+    // Arrange - mock the Cosmos DB create method to throw an error
     const mockError: DbError = {
       message: 'An unknown error occurred',
       code: 500,
     };
-
-    (inputVerified as jest.Mock).mockReturnValue(true);
     (mockContainer.items.create as jest.Mock).mockRejectedValue(mockError);
 
+    // Act - Call the function to test
     const insertDocumentResult = await insertDocument(mockContainer, input);
 
-    // Verify type as DbError
+    // Assert - verify type as DbError
     if (isDbError(insertDocumentResult)) {
       expect(insertDocumentResult.message).toBe(mockError.message);
     } else {
       throw new Error('Result is not of type DbError');
     }
 
-    // Behavior verification: Ensure create method was called with correct arguments
+    // Assert - Behavior verification: Ensure create method was called with correct arguments
     expect(mockContainer.items.create).toHaveBeenCalledTimes(1);
   });
 });
