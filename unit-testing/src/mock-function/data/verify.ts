e14fe7edd92c23f8c1b7e46c619f938f4b7944a3
Dina Berry (MSFT) <diberry@microsoft.com>
2024-08-27 08:17:47 -0700

    Unit testing with mocks (#163)
    * Unit testing with mocks
    
    * gitignore dist
    
    * remove dist
    
    * more gitignore
    
    * Update unit-testing/src/mock-function/lib/insert.spec.ts
    
    Co-authored-by: Maor Leger <maorleger@users.noreply.github.com>
    Signed-off-by: Dina Berry (MSFT) <diberry@microsoft.com>
    
    * edits from Maor's review
    
    * Update tests
    
    * final changes for PR feedback from Maor
    
    * edits
    
    * boilerplate
    
    * update test file
    
    * first set of changes
    
    ---------
    
    Signed-off-by: Dina Berry (MSFT) <diberry@microsoft.com>
    Co-authored-by: Maor Leger <maorleger@users.noreply.github.com>

---
+++ unit-testing/src/mock-function/data/verify.ts
@@ -0,0 +1,7 @@
+// input-verified.ts
+import { validateRawInput } from './model';
+
+export function inputVerified(doc: any): boolean {
+  const result = validateRawInput(doc);
+  return result.length === 0 ? true : false;
+}
---
+++ unit-testing/src/mock-function/index.ts
@@ -0,0 +1,17 @@
+import { connectToContainer } from './data/connect-to-cosmos';
+import { createTestInput } from './data/fake-data';
+import { DbDocument, DbError, VerificationErrors } from './data/model';
+import { insertDocument } from './lib/insert';
+
+async function main(): Promise<DbDocument | DbError | VerificationErrors> {
+  const container = await connectToContainer();
+  const input = createTestInput();
+  return await insertDocument(container, input);
+}
+
+main()
+  .then((doc) => console.log(doc))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
