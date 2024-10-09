e2cb285308fc4baff06098715743dc591c8227ad
Jefin Mathew <56828393+JefinDX@users.noreply.github.com>
2024-10-09 17:30:59 +0530

    Update index.js
    the 'return' statement should be outside the loop.
    
    Signed-off-by: Jefin Mathew <56828393+JefinDX@users.noreply.github.com>

--- nodejs-files/9-exercise-read-write-files/index.js
+++ nodejs-files/9-exercise-read-write-files/index.js
@@ -42,13 +42,12 @@ async function findSalesFiles(folderName) {
           results.push(`${folderName}/${item.name}`);
         }
       }
-
-      return results;
     }
   } catch (error) {
     console.error('Error reading folder:', error.message);
     throw error;
   }
+  return results;
 }
 
 async function main() {
